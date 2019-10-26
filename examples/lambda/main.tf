08223e82c57c021652bfb7f3bfd56462ac32844c
Georgi Tenev <georgi.tenev@naxex-tech.com>
2019-10-26 12:03:57 +0300

    use aws profile

--- examples/lambda/main.tf
+++ examples/lambda/main.tf
@@ -1,6 +1,7 @@
 # Specify the provider and access details
 provider "aws" {
   region = "${var.aws_region}"
+  profile = "progress"
 }
 
 provider "archive" {}
@@ -38,7 +39,7 @@ resource "aws_lambda_function" "lambda" {
 
   role    = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
-  runtime = "python3.6"
+  runtime = "python3.7"
 
   environment {
     variables = {
